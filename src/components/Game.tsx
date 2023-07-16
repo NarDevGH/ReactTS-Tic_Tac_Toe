3038a5b5416b813c162342f32b9807ddb0d5e668
BMateo <bautista.mateo@outlook.com>
2023-07-16 16:25:37 -0300

    add toggle ascending moves history

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,6 +6,7 @@ import { BoardSquares } from "./Board";
 export default function Game() {
     const [history, setHistory] = useState<Array<BoardSquares>>([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState<number>(0);
+    const [ascending, setAscending] = useState<boolean>(true);
 
     const isXTurn = currentMove % 2 === 0;
     const currentSquares = history[currentMove];
@@ -49,12 +50,21 @@ export default function Game() {
         )
     })
 
+    if (!ascending) moves.reverse();
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board isXTurn={isXTurn} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
+                <label>
+                    <input
+                        type='checkbox'
+                        checked={ascending}
+                        onChange={(e) => setAscending(e.target.checked)} />
+                    Ascending
+                </label>
                 <ol>
                     {moves}
                 </ol>
